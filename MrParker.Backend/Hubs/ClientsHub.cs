27fdd52affe510f06a710c1a9939ff726b77fd3c
MSc <maettu@mundms.ch>
2021-10-08 13:15:08 +0200

    v1.0

---
+++ MrParker.Backend/Hubs/ClientsHub.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MrParker.Hubs
+{
+    public class ClientsHub : Hub<Interfaces.ISignalRClient>
+    {
+        public async Task SendMessage(string user, string message)
+        {
+            await Clients.All.ReceiveMessage(user, message);
+        }
+    }
+}
