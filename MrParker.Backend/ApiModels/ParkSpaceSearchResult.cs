479dc6f993ef573a5d7c3c8a9a8c816d52db05c4
Gerhard Hausammann <info@spaark.ch>
2021-10-08 15:37:03 +0200

    Added API-Controller ParkingSpace

---
+++ MrParker.Backend/ApiModels/ParkSpaceSearchResult.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MrParker.ApiModels
+{
+    public class ParkSpaceSearchResult
+    {
+        public string Id { get; set; }
+        public string ProviderName { get; set; }
+        public decimal PositionLat { get; set; }
+        public decimal PositionLong { get; set; }
+        public string Address { get; set; }
+        public string Description { get; set; }
+        public DateTime FromTime { get; set; }
+        public DateTime ToTime { get; set; }
+        public decimal RatePerMinute { get; set; }
+        public string Currency { get; set; }
+        public int Capacity { get; set; }
+    }
+}
---
+++ MrParker.Backend/Controllers/ParkingSpacesController.cs
@@ -0,0 +1,77 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MrParker.ApiModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace MrParker.Controllers
+{
+    [ApiController]
+    public class ParkingSpacesController : ControllerBase
+    {
+        // GET: api/<ParkingSpacesController>
+        [HttpGet]
+        [Route("api/[controller]/search")]
+        public IEnumerable<ParkSpaceSearchResult> Get(string position, DateTime from, DateTime to)
+        {
+            return new[] { 
+                new ParkSpaceSearchResult
+                {
+                    Id = "1",
+                    ProviderName = "EWB",
+                    Address = "Monbijoustrasse 11, \n3011 Bern",
+                    Currency = "CHF",
+                    Description = "Description...",
+                    FromTime = from.AddHours(-1),
+                    ToTime = to.AddHours(6),
+                    PositionLat = 46.944840M,
+                    PositionLong = 7.436910M,
+                    RatePerMinute = .1M,
+                    Capacity = 45
+                },
+                new ParkSpaceSearchResult
+                {
+                    Id = "2",
+                    ProviderName = "Muster Hans",
+                    Address = "Klösterlistutz 20, \n3013 Bern",
+                    Currency = "CHF",
+                    Description = "Description...",
+                    FromTime = from.AddHours(-4),
+                    ToTime = to.AddDays(1),
+                    PositionLat = 46.948580M,
+                    PositionLong = 7.459832M,
+                    RatePerMinute = .15M,
+                    Capacity = 1
+                }
+            };
+        }
+
+        //// GET api/<ParkingSpacesController>/5
+        //[HttpGet("{id}")]
+        //public string Get(int id)
+        //{
+        //    return "value";
+        //}
+
+        //// POST api/<ParkingSpacesController>
+        //[HttpPost]
+        //public void Post([FromBody] string value)
+        //{
+        //}
+
+        //// PUT api/<ParkingSpacesController>/5
+        //[HttpPut("{id}")]
+        //public void Put(int id, [FromBody] string value)
+        //{
+        //}
+
+        //// DELETE api/<ParkingSpacesController>/5
+        //[HttpDelete("{id}")]
+        //public void Delete(int id)
+        //{
+        //}
+    }
+}
