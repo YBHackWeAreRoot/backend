aa1373b8f8e1ec79bf76219b804191719321fd66
MSc <maettu@mundms.ch>
2021-10-08 21:32:52 +0200

    FIX: Dapper -> Update hatte statische Properties drin

--- MrParker.DataAccess/Helpers/Dapper.cs
+++ MrParker.DataAccess/Helpers/Dapper.cs
@@ -21,39 +21,38 @@ namespace MrParker.DataAccess.Helpers
         /// <param name="connection"></param>
         /// <param name="param">An anonymous object with key=value types</param>
         /// <returns>The Id of the updated row. If no row was updated or id was not part of fields, returns null</returns>
-        public static async Task<object> UpdateFieldsAsync<T>(this IDbConnection connection, object param, IDbTransaction transaction = null, int? commandTimeOut = null, CommandType? commandType = null)
+        public static async Task<object> UpdateFieldsAsync<T>(this IDbConnection connection, Dictionary<string, object> param, IDbTransaction transaction = null, int? commandTimeOut = null, CommandType? commandType = null)
         {
             var names = new List<string>();
             Guid id = Guid.Empty;
 
-            names.Add("Currency");
-            Dictionary<string, object> dict = (Dictionary<string, object>)param;
-            dict.TryGetValue("Id", out object guid);
-            id = (Guid)guid;
+            if (param is Dictionary<string, object> dict)
+            { 
+                dict.TryGetValue("Id", out object guid);
+                id = (Guid)guid;
 
-            //foreach (PropertyDescriptor property in TypeDescriptor.GetProperties(param))
-            //{
-            //    if (!"Id".Equals(property.Name, StringComparison.InvariantCultureIgnoreCase))
-            //    {
-            //        names.Add(property.Name);
-            //    }
-            //    else if (property.GetValue(param) is Guid guid)
-            //    { 
-            //        id = guid;
-            //    }
-            //}
+                foreach (KeyValuePair<string, object> kvp in dict)
+                {
+                    if (!"Id".Equals(kvp.Key, StringComparison.InvariantCultureIgnoreCase))
+                    {
+                        names.Add(kvp.Key);
+                    }
+                }
 
-            if (id != Guid.Empty && names.Count > 0)
-            {
-                var sql = string.Format("UPDATE {1} SET {0} WHERE Id=@Id", string.Join(",", names.Select(t => { t = t + "=@" + t; return t; })), typeof(T).Name);
-                if (Debugger.IsAttached)
-                    Trace.WriteLine(string.Format("UpdateFields: {0}", sql));
-                return await connection.ExecuteAsync(sql, param, transaction, commandTimeOut, commandType) > 0 ? id : null;
+                if (id != Guid.Empty && names.Count > 0)
+                {
+                    var sql = string.Format("UPDATE {1} SET {0} WHERE Id=@Id", string.Join(",", names.Select(t => { t = t + "=@" + t; return t; })), typeof(T).Name);
+                    if (Debugger.IsAttached)
+                        Trace.WriteLine(string.Format("UpdateFields: {0}", sql));
+                    return await connection.ExecuteAsync(sql, param, transaction, commandTimeOut, commandType) > 0 ? id : null;
+                }
             }
+
+
             return null;
         }
 
-        public static object UpdateFieldsAsync<T>(this IDbConnection connection, Guid id, object fields, CommandDefinition commandDefinition)
+        public static object UpdateFieldsAsync<T>(this IDbConnection connection, Dictionary<string, object> fields, CommandDefinition commandDefinition)
         {
             return UpdateFieldsAsync<T>(connection, fields, commandDefinition.Transaction, commandDefinition.CommandTimeout, commandDefinition.CommandType);
         }
