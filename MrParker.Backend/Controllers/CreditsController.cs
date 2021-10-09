c5fdf8755c9e38fc9055254ccf328d8054124a23
Gerhard Hausammann <info@spaark.ch>
2021-10-09 09:53:05 +0200

    Credits API Controller

---
+++ MrParker.Backend/Controllers/CreditsController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MrParker.Controllers
+{
+    [ApiController]
+    public class CreditsController : ControllerBase
+    {
+        [HttpGet]
+        [Route("api/[controller]/raw")]
+        public string GetCreditsString()
+        {
+            return @"
+bla
+bla
+";
+        }
+
+        [HttpGet]
+        [Route("[controller]")]
+        public ContentResult GetCreditsHtml()
+        {
+            var template = $@"<!doctype html>
+<html lang=""en"">
+<head>
+    <meta charset=""utf-8"">
+    <title>WeAreRoot - Credits | Hackathon 2021</title>
+</head>
+<body>
+    <pre>
+{GetCreditsString()}
+    </pre>
+</body>
+</html>";
+
+            return base.Content(template, "text/html; charset=utf-8");
+        }
+    }
+}
